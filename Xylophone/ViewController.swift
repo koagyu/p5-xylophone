33fe206038a999c008757098bbf8e223e5a84c61
Istvan Kohalmi <ikhalmi@gmail.com>
2025-09-06 15:07:00 +0200

    first commit - base functionality

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+    
+    var players: [String: AVAudioPlayer] = [:]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        for p in ["A", "B", "C", "D", "E", "F", "G"] {
+            let url = Bundle.main.url(forResource: p, withExtension: "wav")
+            players[p] = try! AVAudioPlayer(contentsOf: url!)
+                
+        }
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let pitch = sender.currentTitle!
+        playSound(pitch: pitch)
+    }
     
+    func playSound(pitch: String) {
+        player = players[pitch]!
+        player.play()
+    }
 
 }
 
